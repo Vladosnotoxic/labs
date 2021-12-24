580f3741001f95c037027f31ce58576ba8a66c3f
Vladosnotoxic <94399193+Vladosnotoxic@users.noreply.github.com>
2021-12-24 10:56:45 +0300

    Update LoginForm.js

--- lab9/src/components/LoginForm.js
+++ lab9/src/components/LoginForm.js
@@ -9,23 +9,29 @@ class LoginForm extends React.Component {
 
     this.onChangeLogin = this.onChangeLogin.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
   }
 
   onChangePassword(event) {
     this.setState({ password: event.target.value });
+    localStorage.setItem(
+      "password",
+      JSON.stringify(this.state.password)
+    );
   }
 
   onChangeLogin(event) {
     this.setState({ login: event.target.value });
+    localStorage.setItem("login", JSON.stringify(this.state.login));
   }
 
   render() {
     return (
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
+      <form onSubmit={this.onSubmit}>
         <p>
           <label>
             {" "}
@@ -50,16 +56,7 @@ class LoginForm extends React.Component {
             />
           </label>
         </p>
-        <Button
-          label="Submit"
-          handleClick={() => {
-            localStorage.setItem("login", JSON.stringify(this.state.login));
-            localStorage.setItem(
-              "password",
-              JSON.stringify(this.state.password)
-            );
-          }}
-        />
+        <input type="submit" value="Отправить" />
       </form>
     );
   }
